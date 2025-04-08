11e129bf4c0b52b89caba15924dbdc77554c2e07
indkhan <mgsukhan@gmail.com>
2025-04-08 19:45:03 +0200

    basic output

--- first.py
+++ first.py
@@ -1,16 +1,37 @@
-from browser_use import Agent, Browser, BrowserConfig
+from browser_use import Agent, Browser, BrowserConfig, Controller, ActionResult
 from dotenv import load_dotenv
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 import asyncio
 
+from pydantic import BaseModel
+
+
 load_dotenv()
+
+class Post(BaseModel):
+    post_title:str
+    post_url:str
+    num_comments: int
+    hours_since_post: int
+
+class Posts(BaseModel):
+    posts:List[Post]
+
+
+
+
 
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
 # gemini-2.5-pro-exp-03-25,gemini-2.0-flash-lite,gemini-2.0-flash
+
+
+# Initialize the controller
+controller = Controller(output_model=Posts)
+
 
 
 browser = Browser(
@@ -21,18 +42,33 @@ browser = Browser(
     )
 )
 
+initial_actions = [
+    {'open_tab':{'url':'https://www.linkedin.com/jobs/'}}
+        ]
 
 async def main():
     agent = Agent(
-        task="go to youtube and search for 'python programming'",
+            task="Find a job on linkedin and search for job related to Software development and print the job description",
         llm=llm,
         save_conversation_path="logs/conversation",
         browser=browser,
+        initial_actions = initial_actions,
+        controller=controller,
     )
-    result = await agent.run()
-    print(result)
-    input("Press Enter to close the browser...")
-    await browser.close()
+    results = await agent.run()
+    result = results.final_result()
+    if result:
+        parsed: Posts = Posts.model_validate_json(result)
+        for post in parsed.posts:
+            print('\n----------------------')
+            print(f'Title:              {post.post_title}')
+            print(f'URL:                {post.post_url}')
+            print(f'COmments:           {post.num_comments}')
+            print(f'Hours since post:   {post.hours_since_post}')
+        else:
+            print('No results')
 
 
-asyncio.run(main())
+
+if __name__ == '__main__':
+    asyncio.run(main())
